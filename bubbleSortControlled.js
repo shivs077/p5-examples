201407f2584ba8a6c963ade057ace6f05fee2fff
zoyron <sgrarora3@gmail.com>
2020-01-16 21:24:49 +0530

    fixed bubbleSort.js and added bubbleSortControlled.js as well

--- bubbleSortControlled.js
+++ bubbleSortControlled.js
@@ -1,7 +1,9 @@
 let values = [];
-let i =  0;
+let i = 0;
+let j = 0;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
   for(let i = 0;i<width;i++){
     values.push(random(height));
   }
@@ -9,24 +11,30 @@ function setup() {
 
 function draw() {
   background(0);
+  for(let k = 0;k<50;k++){
   if(i<values.length){
-    for(let j = 0;j<values.length-i-1;j++){
+    //for(let j = 0;j<values.length-i-1;j++){
       let temp = values[j];
       if(values[j] > values[j+1]){
         values[j] = values[j+1];
         values[j+1] = temp;
       }
+    j++;
+    //}
+    if(j>=values.length-i-1){
+      j = 0;
+      i++;
     }
   }
   else{
     console.log("Finished Sorting");
     noLoop();
   }
-  i++;
+  }
   for(let i = 0;i<values.length;i++){
-   stroke(random(255),random(255),random(255));
-    //noStroke();
-    //fill(random(255),random(255),random(255));
+   //stroke(23,69,207);
+    //fill(169);
+    stroke(random(255),random(255),random(255));
     rect(i , height, 1, -values[i]);
   }
 }
