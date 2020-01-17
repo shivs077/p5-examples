1a9f5945e867317440b8d42646644c6b28a5a3af
zoyron <sgrarora3@gmail.com>
2020-01-17 14:11:12 +0530

    added the maurerRose.js file. It works fine.

---
+++ maurerRose.js
@@ -0,0 +1,59 @@
+let xSpeed = 5;
+let ySpeed = -3;
+let xPos;
+let yPos;
+let slider;
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    xPos = width/2;
+    yPos = height/2;
+    angleMode(DEGREES);
+   // slider = createSlider(10,400,50,2);
+    //scale(10);
+}
+function windowResized(){
+    resizeCanvas(windowWidth,windowHeight);
+}
+let n = 6;
+let d = 71;
+function draw(){
+    background(0);
+    xPos+=xSpeed;
+    yPos+=ySpeed;
+    if(xPos>=width || xPos<=0){
+        xSpeed*=-1;
+    }
+    if(yPos>=height || yPos<=0){
+        ySpeed*=-1;
+    }
+    translate(xPos,yPos);
+    //stroke(random(255),random(255),random(255));
+    stroke(55);
+    noFill();
+    // moveShape();
+    // bounceShape();
+    let rTemp = 159;
+    beginShape();
+    for(let a = 0;a<361;a++){
+        
+        let r = rTemp*sin(n*a*d); //radius of the circle
+        let x = r*cos(d*a);
+        let y = r*sin(d*a);
+        vertex(x,y);
+    }
+    endShape(CLOSE);
+}
+
+// function bounceShape(){
+//     if(xPos>=width || xPos<=0){
+// 	xSpeed*=-1;
+//     }
+//     if(yPos>=height || yHeight<=0){
+// 	ySpeed*=-1;
+//     }
+// }
+
+// function moveShape(){
+//     xPos+=xSpeed;
+//     yPos+=ySPeed;
+// }
