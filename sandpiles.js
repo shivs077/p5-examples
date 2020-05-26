ae3b7027ece0f52f0d411d5eb363b21f964e0b9c
zoyron <sgrarora3@gmail.com>
2020-05-26 12:18:29 +0530

    created sandpiles, not worth posting though

--- sandpiles.js
+++ sandpiles.js
@@ -1,28 +1,82 @@
+let defaultColor = [255, 0, 0];
+let colors = [
+  [255, 255, 0],
+  [0, 185, 63],
+  [0, 104, 255],
+  [122, 0, 229],
+];
+
 let sandpiles;
+let nextpiles;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  sandpiles = new Array(width);
-  for (let i = 0; i < width; i++) {
-    sandpiles[i] = new Array(height);
+  createCanvas(600, 600);
+  pixelDensity(1);
+  scale(10);
+  sandpiles = new Array(width).fill().map((i) => new Array(height).fill(0));
+  nextpiles = new Array(width).fill().map((i) => new Array(height).fill(0));
+
+  sandpiles[width / 2][height / 2] = 1000000000;
+
+  background("#0f0f0f");
+}
+
+function topple() {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      nextpiles[x][y] = sandpiles[x][y];
+    }
   }
-  sandpiles[width / 2][height / 2] = 4;
+
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let num = sandpiles[x][y];
+      if (num >= 4) {
+        nextpiles[x][y] -= 4;
+        if (x + 1 < width) nextpiles[x + 1][y]++;
+        if (x - 1 >= 0) nextpiles[x - 1][y]++;
+        if (y + 1 < height) nextpiles[x][y + 1]++;
+        if (y - 1 >= 0) nextpiles[x][y - 1]++;
+      }
+    }
+  }
+
+  let tmp = sandpiles;
+  sandpiles = nextpiles;
+  nextpiles = tmp;
 }
 
 function render() {
   loadPixels();
-  for (let x = 0; x < windowWidth; x++) {
-    for (let y = 0; y < windowHeight; y++) {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
       let num = sandpiles[x][y];
-      let newColor = color(255);
-      if (num == 0) newColor = color(0);
-      else if (num == 1) newColor = color(255, 0, 255);
-      else if (num == 2) newColor = color(0, 255, 255);
-      else if (num == 3) newColor = color(255, 255, 0);
-      pixels[x + y * width] = newColor;
+      let col = defaultColor;
+      if (num == 0) {
+        col = "#0f0f0f";
+      } else if (num == 1) {
+        col = colors[1];
+      } else if (num == 2) {
+        col = colors[2];
+      } else if (num == 3) {
+        col = colors[3];
+      }
+
+      let pix = (x + y * width) * 4;
+      pixels[pix] = col[0];
+      pixels[pix + 1] = col[1];
+      pixels[pix + 2] = col[2];
+      // pixels[pix + 3] = 255;
     }
   }
+
   updatePixels();
 }
 
-function draw() {}
+function draw() {
+  render();
+
+  for (let i = 0; i < 500; i++) {
+    topple();
+  }
+}
