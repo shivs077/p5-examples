89fb96b594504acf96d13b5945dad203a046a670
zoyron <sgrarora3@gmail.com>
2020-07-04 15:50:53 +0530

    finallised color as well

--- phyllotaxis.js
+++ phyllotaxis.js
@@ -3,22 +3,24 @@ let c = 3; // this is the scalling factor
 let xoff = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("#5F4B8BFF");
+  background("#ecc19c");
   angleMode(DEGREES);
 }
 
 function draw() {
   translate(width / 2, height / 2);
   for (let i = 1; i <= 6; i++) {
-    let a = 137.5 * n;
+    let a = 201.113 * n;
     let r = c * sqrt(n);
     let x = r * cos(a);
     let y = r * sin(a);
-    let col = map(noise(xoff), 0, 1, 0, 190);
-    let col1 = map(noise(a), 0, 1, 0, 190);
-    let col2 = map(noise(a), 0, 1, 0, 100);
-    fill("#E69A8DFF");
-    circle(x, y, 25);
+    let col = map(noise(xoff), 0, 1, 0, 50);
+    let col1 = map(noise(xoff), 0, 1, 0, 50);
+    let col2 = map(noise(xoff), 0, 1, 0, 50);
+    stroke("#ecc19c");
+    // fill(col, col1, col2);
+    fill("#1e3d59");
+    circle(y, x, 25);
     n++;
     xoff += 0.01;
   }
