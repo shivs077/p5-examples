777c20a1c83491f84a775bd62258251016d47a7e
zoyron <sgrarora3@gmail.com>
2020-07-04 15:51:40 +0530

    removed perlin noise from the color

--- phyllotaxis.js
+++ phyllotaxis.js
@@ -1,6 +1,5 @@
 let n = 0; // this is the current number of particle
 let c = 3; // this is the scalling factor
-let xoff = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("#ecc19c");
@@ -14,14 +13,9 @@ function draw() {
     let r = c * sqrt(n);
     let x = r * cos(a);
     let y = r * sin(a);
-    let col = map(noise(xoff), 0, 1, 0, 50);
-    let col1 = map(noise(xoff), 0, 1, 0, 50);
-    let col2 = map(noise(xoff), 0, 1, 0, 50);
     stroke("#ecc19c");
-    // fill(col, col1, col2);
     fill("#1e3d59");
     circle(y, x, 25);
     n++;
-    xoff += 0.01;
   }
 }
